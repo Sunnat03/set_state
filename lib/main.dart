f783a95c1d8b82b4a5dedad10f77e19fd1bcc039
Sunnat03 <sunnatmaxmayusupov02@gmail.com>
2022-08-26 17:13:23 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:pattern_set_state/page/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Pattern",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/models/post_model.dart
@@ -0,0 +1,21 @@
+class Post {
+  int id;
+  String title;
+  String body;
+  int userId;
+
+  Post({required this.id, required this.title, required this.body, required this.userId});
+
+  Post.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        body = json['body'],
+        userId = json['userId'];
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'body': body,
+    'userId': userId,
+  };
+}
